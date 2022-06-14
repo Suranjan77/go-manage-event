e45ac7d9ff3516c1387a12eb2637dbea80cbbba2
suranjan <7suranjanpoudel7@gmail.com>
2022-06-14 08:18:24 +0545

    feat: Env wise config implemented using viper

---
+++ utils/application_properties.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+var P Properties
+
+type Properties struct {
+	Server Server
+	DataSource DataSource
+}
+
+type DataSource struct {
+	Host string
+	Port string
+	UserName string
+	Password string
+}
+
+type Server struct {
+	Port string
+}
+
+func init() {
+	if os.Getenv("ENVIRONMENT") == "PROD" {
+		viper.SetConfigName("config-prod")
+	} else {
+		viper.SetConfigName("config-dev")
+	}
+
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if e, ok := err.(viper.ConfigFileNotFoundError); ok {
+			panic("Application configuration file not found")
+		} else {
+			panic("Error reading application configuration file\n" + e.Error())
+		}
+	}
+
+	P = Properties{}
+
+	err := viper.Unmarshal(&P)
+
+	if err != nil {
+		panic("Failed to load properties\n" + err.Error())
+	}
+
+	fmt.Println("Application properties loaded.")
+}
